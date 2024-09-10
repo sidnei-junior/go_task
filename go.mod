53dde7a3a54f1fc3403a176c43dec88e0171155a
Sidnei Araújo Júnior <sidnei.ifrn@gmail.com>
2024-09-10 09:58:45 +0000

    feat: create a simple route to say hello

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sidnei-junior/go_task
 
 go 1.23.0
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func helloHandler( w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello, World!"))
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	greeting := fmt.Sprintf("Hello, %s!", name)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(greeting))
+}
 
 func main() {
-    fmt.Println("Hello, Windows!")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
+	r.HandleFunc("/greet/{name}", greetHandler).Methods("GET")
+
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
